29c3fd71841daadbfc98633dcd45aecedf1699d2
Waignier Stéphane <stephwgr59@gmail.com>
2025-10-27 22:24:24 +0100

    add project application PrettyTimer

---
+++ PrettyTimer_/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace PrettyTimer;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+    
+
+}
